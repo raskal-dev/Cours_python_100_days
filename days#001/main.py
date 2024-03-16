f8500298e00aeacd183139ce85b425c62f9946d7
raskal-dev <kalvinrasamyzo@gmail.com>
2024-03-16 07:26:34 +0300

    days#001

--- days#001/main.py
+++ days#001/main.py
@@ -3,3 +3,10 @@
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
+
+# input("What is your name? ")
+
+YOUR_NAME = input("What is your name? \n--> ")
+
+print("Hello " + YOUR_NAME + " !")
